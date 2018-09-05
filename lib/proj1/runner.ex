b6342aa65d6d6d00b552bcb4e76c4991d651dcd8
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-09-05 12:52:40 -0400

    Add runner module to perform the search within a range

---
+++ lib/proj1/runner.ex
@@ -0,0 +1,18 @@
+defmodule Runner do
+  def run(first, last, len) do
+    list = Enum.to_list first..last
+    result = Enum.filter list, fn starting_point -> search(starting_point, len) end
+    result
+  end
+
+  defp search(starting_point, len) do
+    #start squaring numbers and summing them up
+    range = starting_point..(starting_point + len - 1)
+    sum = Enum.reduce(range, 0, fn (x, acc) -> x*x + acc end )
+    is_perfect_square?(sum)
+  end
+
+  defp is_perfect_square?(n) do
+    :math.sqrt(n) |> :erlang.trunc |> :math.pow(2) == n
+  end
+end
