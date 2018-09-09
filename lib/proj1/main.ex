26bc9c82a2a7db440ec1e3d81aa002fff04519be
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-09-09 12:03:11 -0400

    Add Task Supervisor.

--- lib/proj1/main.ex
+++ lib/proj1/main.ex
@@ -1,35 +1,19 @@
 defmodule M do
-
   def main(n,k) do
-
-    workers = 500
+    workers = 100
 
     subproblem_size = n/workers |> Float.ceil |> :erlang.trunc
 
     subproblems = Enum.chunk_every(1..n, subproblem_size)
 
-    :erlang.statistics(:runtime)
-    :erlang.statistics(:wall_clock)
-
-    #initialize a genserver
-    {:ok, registry} = Proj1.Registry.start_link([])
-
     tasks = Enum.map subproblems, fn subproblem ->
-      Task.async(fn -> Runner.run(subproblem, k, registry) end)
+      Task.Supervisor.async(Proj1.TaskSupervisor, fn -> Runner.run(subproblem, k, Proj1.Registry) end)
     end
 
     Enum.each tasks, fn task ->
       Task.await(task, :infinity)
     end
 
-    #get the results
-    IO.inspect :sys.get_state(registry)
-
-    {_, t1} = :erlang.statistics(:runtime)
-    {_, t2} = :erlang.statistics(:wall_clock)
-
-    IO.puts (t1/t2)
-    
+    IO.inspect :sys.get_state(Proj1.Registry)
   end
-
 end
